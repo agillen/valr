5696a0844b977bf398642a9d74050c7396b107a0
Austin Gillen <austingillen@gmail.com>
2017-06-05 13:29:13 -0600

    remove read.table from namespace and valr-package.r

--- R/valr-package.r
+++ R/valr-package.r
@@ -26,7 +26,7 @@
 #' @importFrom readr read_tsv col_integer col_character col_double
 #' @importFrom stringr str_replace str_split str_c str_length
 #' @importFrom lazyeval lazy_dots
-#' @importFrom utils head tail read.table
+#' @importFrom utils head tail
 #' @importFrom stats fisher.test na.omit
 #' @import ggplot2
 #' @import dplyr
